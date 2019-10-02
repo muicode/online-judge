73196bf0c8e1921d9a76cea0f73825e7248d1a11
Jii Eu <jeyeyeu@pm.me>
2019-10-01 19:50:55 -0700

    c moved to cpp

--- problem1/problem1.cpp
+++ problem1/problem1.cpp
@@ -18,7 +18,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE (20)
+#include <map>
+
+using namespace std;
+
+#define SIZE (100)
 
 void bruteforce(const int * const arr, const int k)
 {
@@ -49,54 +53,25 @@ int isInclude(const int * const arr, const int n)
     return 0;
 }
 
-void twopass(const int * const arr, const int k)
-{
-    /* 주어진 배열의 크기 최대가 SIZE라고 가정 */
-    int *m = malloc(sizeof(*m) * SIZE);
-
-    for (size_t i=0; arr[i] != '\0'; ++i) 
-    {
-        m[i] = arr[i];
-    }
-
-    for (size_t i=0; arr[i] != '\0'; ++i)
-    {
-        int index = isInclude(m, k - arr[i]);
-        if (index != i && index)
-        {
-            printf("found: %d + %d = %d\n", k - arr[i], arr[i], k);
-            free(m);
-            return;
-        }
-    }
-
-    free(m);
-    printf("none exists\n");
-}
-
 void onepass(const int * const arr, const int k)
 {
-    /* 주어진 배열의 크기 최대가 SIZE라고 가정 */
-    int *m = malloc(sizeof(*m) * SIZE);
+    map<int, int> mymap;
 
-    for (size_t i=0; arr[i] != '\0'; ++i)
+    for (size_t index = 0; index < SIZE; ++index)
     {
-        int index = isInclude(m, k - arr[i]);
-        if (index != i && index)
+        int complement = k - arr[index];
+
+        if (mymap.find(complement) != mymap.end() && mymap[complement] != index)
         {
-            printf("found: %d + %d = %d\n", k - arr[i], arr[i], k);
-            free(m);
-            return;
+                printf("found: %d + %d = %d\n", complement, arr[index], k);
+                return;
         }
         else
         {
-            m[i] = arr[i];
+            mymap[arr[index]] = index;
         }
     }
-
-    free(m);
     printf("none exists\n");
-
 }
 
 int main(void)
@@ -108,15 +83,13 @@ int main(void)
 
     k = (rand() % 20) + 1;
 
-    for (size_t i=0; i<SIZE; ++i)
+    for (size_t i = 0; i < SIZE; ++i)
     {
-        arr[i] = (rand() % SIZE) + 1;
-        printf("index %2ld: %2d\n", i, arr[i]);
+        arr[i] = rand() % SIZE + 1;
     }
 
     printf("k: %ld\n", k);
     bruteforce(arr, k);
-    twopass(arr, k);
     onepass(arr, k);
 
     return 0;
--- problem1/problem1.rb
+++ problem1/problem1.rb
@@ -31,52 +31,29 @@ def bruteforce(arr, k)
     puts "none exists"
 end
 
-def two_pass(arr, k)
-    m = []
-
-    arr.each do |n|
-        m.insert(n)
-    end
-
-    arr.each do |n|
-        if m.include? (k - n)
-            puts "found: #{k - n} + #{n} = #{k}"
-            return
-        end
-    end
-    puts "none exists"
-end
-
 def one_pass(arr, k)
-    m = []
+    m = Hash.new(0)
 
     arr.each do |n|
-        if m.include? (k - n)
+        if m.has_key? (k - n)
             puts "found: #{k - n} + #{n} = #{k}"
             return
         else
-            m.insert(n)
+            m[n] = (k - n)
         end
     end
     puts "none exists"
 end
 
-size = 10000
-arr = [*0 ... size].shuffle
-k = (rand % size) + 1
-
+size = 99999
+arr = [*1 ... size].shuffle
+k = rand(size)
+puts "k = #{k}"
 time = Benchmark.realtime {
     bruteforce(arr, k)
 }
 puts "#{time} seconds"
 puts
-
-time = Benchmark.realtime {
-    two_pass(arr, k)
-}
-puts "#{time} seconds"
-puts
-
 time = Benchmark.realtime {
     one_pass(arr, k)
 }
