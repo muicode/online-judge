2c563d59469310ac38de2ec6aa709409c49d4d59
Jii Eu <public.mui@gmail.com>
2019-10-15 15:15:27 -0700

    dcp1 readme

--- problem1/dcp1.rb
+++ problem1/dcp1.rb
@@ -1,19 +1,3 @@
-###############################################################################
-# 2019-09-18
-###############################################################################
-# DCP #1 
-#
-# This problem was recently asked by Google.
-#
-# Given a list of numbers and a number k, return whether any two numbers from 
-# the list add up to k.
-#
-# For example, given [10, 15, 3, 7] and k of 17, 
-# return true since 10 + 7 is 17.
-#
-# Bonus: Can you do this in one pass?
-###############################################################################
-
 require 'benchmark'
 
 def bruteforce(arr, k)
