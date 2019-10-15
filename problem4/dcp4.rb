dde5c4ef9c7a0e2cb1c6bd419646a255d2b0db30
Jii Eu <jeyeyeu@pm.me>
2019-10-15 15:12:42 -0700

    dcp4 readme

--- problem4/dcp4.rb
+++ problem4/dcp4.rb
@@ -1,19 +1,3 @@
-###############################################################################
-# 2019-09-21
-###############################################################################
-# This problem was asked by Stripe.
-#
-# Given an array of integers, find the first missing positive integer in 
-# linear time and constant space. In other words, find the lowest positive 
-# integer that does not exist in the array. The array can contain duplicates 
-# and negative numbers as well.
-#
-# For example, the input [3, 4, -1, 1] should give 2. 
-# The input [1, 2, 0] should give 3.
-#
-# You can modify the input array in-place.
-###############################################################################
-
 # Linear Time
 # Linear Space
 def dcp4(input)
