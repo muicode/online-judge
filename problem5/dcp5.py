ccedc8c4ded5cf8a768f7dce6d59897229393221
Jii Eu <jeyeyeu@pm.me>
2019-10-25 17:39:44 -0700

    commented out lambda

--- problem5/dcp5.py
+++ problem5/dcp5.py
@@ -4,10 +4,16 @@ def cons(a, b):
     return pair
 
 def car(pair):
-    return pair(lambda x, y: x)
+#    return pair(lambda x, y: x)
+    def left(left, right):
+        return left
+    return pair(left)
 
 def cdr(pair):
-    return pair(lambda x, y: y)
+#    return pair(lambda x, y: y)
+    def right(left, right):
+        return right
+    return pair(right)
 
 print(car(cons(3, 4)))
 print(cdr(cons(3, 4)))
