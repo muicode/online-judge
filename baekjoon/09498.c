42c965196f84ea8096efc28be567380762f0e26b
Jii Eu <jeyeyeu@pm.me>
2020-01-30 22:59:14 -0800

    baekjoon basic problems

---
+++ baekjoon/09498.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int score;
+    scanf("%d", &score);
+    printf("%c\n", (score>=90) ? 'A' :
+                   (score>=80) ? 'B' :
+                   (score>=70) ? 'C' :
+                   (score>=60) ? 'D' : 'F');
+    return 0;
+}
