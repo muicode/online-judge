7ab7c30c77241f18a7a2e8044e8ec5f699a671bd
Jii Eu <jeyeyeu@pm.me>
2020-05-05 14:51:56 +0900

    dcp9 code fixed

--- problem9/dcp9.rb
+++ problem9/dcp9.rb
@@ -8,9 +8,19 @@ def dcp9(nums)
     excl = temp
   end
 
-  incl
+  return excl > incl ? excl : incl
 end
 
-nums = [2, 4, 6, 2, 5]
-nums2 = [4, 1, 1, 4, 2, 1]
-puts dcp9 nums2
+testcase = [
+  # [testcase, expected value]
+  [ [2, 4, 6, 2, 5],   13 ],
+  [ [4, 1, 1, 4, 2, 1], 9 ], 
+  [ [5, 1, 1, 5],      10 ],
+  [ [5, 1],             5 ],
+  [ [5],                5 ],
+  [ [],                 0 ]
+]
+
+for t in testcase
+  puts "expected: #{t[1]} and got #{dcp9 t[0]}"
+end
