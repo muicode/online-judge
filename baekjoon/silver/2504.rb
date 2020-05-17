f346be8cebd84f7dc8226ff308f8bfeac18e3a81
Jii Eu <jeyeyeu@pm.me>
2020-05-17 20:45:51 +0900

    boj 2504 problem solved

--- baekjoon/silver/2504.rb
+++ baekjoon/silver/2504.rb
@@ -1,83 +1,52 @@
+$open = [0,0]
+$close = [0,0]
+
+def evaluate(c, stack)
+  p = stack.pop
+
+  if c == ')' and p == '('
+    stack.push(2)
+  elsif c == ']' and p == '['
+    stack.push(3)
+  else
+    val = 0
+    while p != '[' and p!= '('
+      val += (p * (c==')' ? 2 : 3))
+      p = stack.pop
+      if (c == ')' and p == '[') or (c == ']' and p == '(')
+        return -1
+      end
+    end
+    stack.push(val)
+  end
+end
+
 s = gets.chomp
-# if odd, not valid
-if ((s.size)&1) == 1
-  puts 0
+ans = nil
+if (s.size&1)==1 
+  $close[0] = 1
 elsif s[0] == ')' or s[0] == ']'
-  puts 0
+  $close[0] = 1
+elsif s.empty?
+  $close[0] = 1
 else
   stack = []
+  size = s.size
 
-  for i in 0...s.size
+  for i in 0...size
     c = s[i]
     if c == '(' or c == '['
+      c == '(' ? $open[0]+=1 : $open[1]+=1
       stack.push(c)
-    elsif c == ')'
-      if stack.empty?
-        puts 0
-        return
-      end
-
-      p = stack.pop
-      if p == '('
-        stack.push(2)
-      elsif p == '['
-        puts 0
-        return
-      else
-        n = p 
-        p = stack.pop
-        if p == '('
-          stack.push(n*2)
-        elsif p == '[' or p == ')'
-          puts 0
-          return
-        else
-          if p == nil
-            puts 0
-            return
-          end
-          stack.push(n+p)
-        end
-      end
-    elsif c == ']'
-      if stack.empty?
-        puts 0
-        return
-      end
-
-      p = stack.pop
-      if p == '['
-        stack.push(3)
-      elsif p == '('
-        puts 0
-        return
-      else
-        n = p
-        p = stack.pop
-        if p == '['
-          stack.push(n*3)
-        elsif p == '(' or p == ']'
-          puts 0
-          return
-        else
-          if p == nil
-            puts 0
-            return
-          end
-          stack.push(n+p)
-        end
+    else
+      c == ')' ? $close[0]+=1 : $close[1]+=1
+      break if $close.sum > $open.sum
+      if evaluate(c, stack) == -1
+        $close[0] += ($open.sum + 1)
+        break
       end
     end
   end
-
-  p = stack.shift
-  stack.unshift(p2)
-  if p == '('
-    stack.push(stack.shift*2)
-  elsif p == '['
-    stack.push(stack.shift*3)
-  else
-    stack.push(p)
-  end
-  puts stack.sum
 end
+
+puts ($open[0] != $close[0] or $open[1] != $close[1]) ? 0 : stack.sum
