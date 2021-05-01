2c51e528ff9f11aaf392aae6c3496ea1e8e6d1ee
Jii Eu <jeyeyeu@pm.me>
2021-05-01 10:46:54 +0900

    10871 added

---
+++ baekjoon/10871.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  int n, x;
+  cin >> n >> x;
+  for(int i=0; i<n; ++i) {
+    int t;
+    cin >> t;
+    if(t<x) cout << t << ' ';
+  }
+  return 0;
+}
