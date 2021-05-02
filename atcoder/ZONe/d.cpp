5c27c7d683dded28865b38cdf2aad67374b7413f
Jii Eu <jeyeyeu@pm.me>
2021-05-02 10:43:27 +0900

    d solved

--- atcoder/ZONe/d.cpp
+++ atcoder/ZONe/d.cpp
@@ -15,25 +15,35 @@ int main(){
   ios; cfs(15);
   string s; cin >> s;
   vector<char> t;
+  vector<char> t2;
+  vector<char> t3;
+  vector<char> t4;
+  bool rev = false;
 
-  for(int i=0; i<s.length(); ++i) {
-    if(s[i]=='R') {
-      reverse(t.begin(), t.end());
-    } else {
-      t.pb(s[i]);
-    }
+  for(char c : s) {
+    if(c=='R') rev = !rev;
+    else {
+      if(rev) t2.pb(c);
+      else t.pb(c);
+     }
   }
 
-  for(int i=0;i<t.size(); ++i){
-    if(t[i] == t[i+1]){
-      t[i] = t[i+1] = '\0';
-      t.erase(remove(t.begin(), t.end(),'\0'), t.end());
-      i -= 2;
-    }
-  }
+  for(int i=t2.size()-1;i>=0;--i)
+    t3.pb(t2[i]);
+  for(char x : t)
+    t3.pb(x);
 
-  for(int i=0; i<t.size(); ++i)
-    cout << t[i];
-  cout << endl;
-  return 0;
+  if(t3.size())
+    t4.pb(t3[0]);
+  for(int i=1; i<t3.size(); ++i) {
+    if(t4.back() != t3[i]) t4.pb(t3[i]);
+    else t4.pop_back();
+  }
+  
+  if(rev)
+    for(int i=t4.size()-1; i>=0; --i) cout << t4[i];
+  else
+    for(char c : t4) cout << c ;
+  cout<<endl;
+ return 0;
 }
