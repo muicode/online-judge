083779ac9700b655137aec183f800a27c5cf647f
Jii Eu <jeyeyeu@pm.me>
2021-05-03 15:15:32 +0900

    Add: 10951, 10952, 11021, 11022, 2438, 2439, 2741, and 2742

---
+++ baekjoon/2742.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int a;
+  cin >> a;
+  for(int i=a; i>=1; --i) printf("%d\n", i);
+
+  return 0;
+}
