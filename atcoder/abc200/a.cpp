cfdac349f4c9a720cb581d2f272eada86ada0b28
Jii Eu <jeyeyeu@pm.me>
2021-05-08 21:22:31 +0900

    A problem solved

---
+++ atcoder/abc200/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define rep(i,n) for(ll i=0;i<(n);++i)
+#define P pair<int,int>
+#define pb push_back
+#define vi vector<int>
+
+#define ios cin.tie(NULL), ios_base::sync_with_stdio(false)
+#define cfs(n) cout<<fixed<<setprecision((n))
+#define endl '\n'
+
+int main(){
+  ios; cfs(15);
+  int n;
+  scanf("%d", &n);
+  printf("%d\n", (n/100) + !!(n%100));
+  return 0;
+}
