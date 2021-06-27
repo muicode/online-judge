bc4392c56394b94a1570878802cdcaeee18b90da
Jii Eu <jeyeyeu@pm.me>
2021-06-27 09:29:38 +0900

    Baekjoon branch added

--- leetcode/easy/1108/1108.js
+++
@@ -1,8 +0,0 @@
-/**
- * Runtime: 68 ms, faster than 95.34%
- * Memory Usage: 38.6 MB, less than 24.72%
- */
-var defangIPaddr = function(address) {
-    const regex = /\./gi;
-    return address.replace(regex, '[.]');
-};
